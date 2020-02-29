f780bcb964e2ee7405964d6eb06745e39324b22c
Jon Fobert <jonrfobert@gmail.com>
2020-02-28 20:35:11 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const canvas = document.querySelector('.game');
+const context = canvas.getContext('2d');
+
+context.clearRect(0, 0, 960, 540);
+
+context.fillStyle = "#EE6C4D";
+context.fillRect(20, 20, 40, 40);
